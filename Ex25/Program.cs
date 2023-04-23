c9ee2e95feed023f1ba0b0f15513a1293d178dc2
pogorelov <89607888555@mail.ru>
2023-04-23 23:56:29 +0700

    Ex25 edit_01

--- Ex25/Program.cs
+++ Ex25/Program.cs
@@ -1,7 +1,7 @@
 ﻿int a = GetUserNumber($"Введите число A: ", "ОШИБКА! Вы ввели некорректные значения!");
 int b = GetUserNumber($"Введите число B: ", "ОШИБКА! Вы ввели некорректные значения!");
 
-double pow = Math.Pow(a, b);
+double pow = GetPow(a, b);
 Console.WriteLine($"{a}, {b} -> {pow}");
 
 int GetUserNumber(string message, string errorMessage)
@@ -15,4 +15,15 @@ int GetUserNumber(string message, string errorMessage)
         }
         else Console.WriteLine(errorMessage);
     }
+}
+
+int GetPow(int a, int b)
+{
+int result = a;
+    while (b > 1)
+    {
+        result *= a;
+        b--;
+    }
+    return result;
 }
