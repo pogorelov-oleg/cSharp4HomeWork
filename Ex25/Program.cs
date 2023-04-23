58e0387cf474baf02933b3974dc4795a0625864e
pogorelov <89607888555@mail.ru>
2023-04-23 23:29:43 +0700

    Ex25 done

---
+++ Ex25/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = GetUserNumber($"Введите число A: ", "ОШИБКА! Вы ввели некорректные значения!");
+int b = GetUserNumber($"Введите число B: ", "ОШИБКА! Вы ввели некорректные значения!");
+
+double pow = Math.Pow(a, b);
+Console.WriteLine($"{a}, {b} -> {pow}");
+
+int GetUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
