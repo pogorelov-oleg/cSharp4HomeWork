c3aabc98e3a328d2f221f8d6dd9cb13703ccffd2
pogorelov <89607888555@mail.ru>
2023-05-04 14:27:43 +0700

    Ex50 done

---
+++ Ex50/Program.cs
@@ -0,0 +1,58 @@
+﻿int i = GetUserNumber($"Введите строку: ", "ОШИБКА! Вы ввели некорректные значения!");
+int j = GetUserNumber($"Введите столбец: ", "ОШИБКА! Вы ввели некорректные значения!");
+int m = 4;
+int n = 6;
+
+int[,] array = GetRandomArray(m, n);
+PrintArray(array);
+Console.WriteLine();
+if (ChekArrayIndex(array, i, j))
+{
+    Console.WriteLine($"В ячейке [{i}, {j}] находится число {array[i, j]}");
+}
+else
+    Console.WriteLine($"В данном массиве нет ячейки [{i}, {j}]");
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userInput) && userInput >= 0)  //добавил проверку, чтобы пользователь не ввел значение < 0
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
+
+int[,] GetRandomArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(0, 10 + 1);
+        }
+    }
+    return array;
+}
+
+bool ChekArrayIndex(int[,] array, int i, int j)
+{
+    return i < array.GetLength(0) && j < array.GetLength(1);
+}
