fbe81f30eeb32becac6dc6dbc0d666409d4be856
pogorelov <89607888555@mail.ru>
2023-05-26 15:06:21 +0700

    Ex64 done

---
+++ Ex64/Program.cs
@@ -0,0 +1,31 @@
+﻿while (true)
+{
+    int n = GetUserNumber("Введите число N: ", "Error");
+    int finalDig = GetUserNumber("Введите последнее число: ", "Error");
+    if (n > finalDig)
+    {
+        string res = PrintNaturalNums(n, finalDig);
+        Console.WriteLine(res);
+        break;
+    }
+    else Console.WriteLine("Error! Конечное число должно быть меньше значения N");
+}
+
+int GetUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
+
+string PrintNaturalNums(int n, int finalDig)
+{
+    if (n == finalDig) return n.ToString();
+    return n + " " + PrintNaturalNums(n - 1, finalDig);
+}
