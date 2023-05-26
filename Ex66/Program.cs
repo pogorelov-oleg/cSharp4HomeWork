9f5e48a6cd620dafe6d9ab8e5b0a47e539ba992e
pogorelov <89607888555@mail.ru>
2023-05-26 15:18:51 +0700

    Ex66 done

---
+++ Ex66/Program.cs
@@ -0,0 +1,32 @@
+﻿while (true)
+{
+    int m = GetUserNumber("Введите число M: ", "Error");
+    int n = GetUserNumber("Введите число N: ", "Error");
+    if (m < n)
+    {
+        int res = GetElementsSum(m, n);
+        Console.WriteLine(res);
+        break;
+    }
+    else Console.WriteLine("Error! Значение M должно быть меньше значения N");
+}
+
+int GetUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine(errorMessage);
+    }
+}
+
+int GetElementsSum(int m, int n)
+{
+    if (m == n) return n;
+    return m + GetElementsSum(m + 1, n);
+}
+
